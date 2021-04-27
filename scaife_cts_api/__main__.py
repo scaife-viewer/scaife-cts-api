b32f0a7a36204a123a35c00a1ef51af29e8cc3d7
Jacob Wegner <jwegner@eldarion.com>
2021-04-27 10:57:25 -0500

    Refactor tarball loading and metadata output
    Prefer the tarball URL that contains a release passing HookTest, if present

--- scaife_cts_api/__main__.py
+++ scaife_cts_api/__main__.py
@@ -6,7 +6,7 @@ from collections import defaultdict
 import click
 
 from . import resolver
-from .data import ROOT_DIR_PATH, load_repo, resolve_commit
+from .data import ROOT_DIR_PATH, load_repo
 
 
 @click.group()
@@ -31,10 +31,10 @@ def loadcorpus(root_dir):
         repos = json.loads(fp.read())
     fs = {}
     with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-        for repo, ref in repos.items():
+        for repo, data in repos.items():
             dest = os.path.join(root_dir, "data")
-            f = executor.submit(do_load_repo, repo, ref, dest)
-            fs[f] = (repo, ref)
+            f = executor.submit(do_load_repo, repo, data, dest)
+            fs[f] = (repo, data["ref"])
         for f in concurrent.futures.as_completed(fs):
             repo, ref = fs[f]
             data = f.result()
@@ -45,24 +45,25 @@ def loadcorpus(root_dir):
 
 
 def write_repo_metadata(repo, data, dest):
-    sv_metadata_path = os.path.join(dest, data["tarball_path"], ".scaife-viewer.json")
-    metadata = {
-        "repo": repo,
-        "sha": data["sha"],
-    }
-    json.dump(metadata, open(sv_metadata_path, "w"), indent=2)
+    tarball_path = data.pop("tarball_path")
+    sv_metadata_path = os.path.join(dest, tarball_path, ".scaife-viewer.json")
+    json.dump(data, open(sv_metadata_path, "w"), indent=2)
 
 
-def do_load_repo(repo, ref, dest):
-    sha = resolve_commit(repo, ref)
+def do_load_repo(repo, data, dest):
+    ref = data["ref"]
+    sha = data["sha"]
     tarball_url = f"https://api.github.com/repos/{repo}/tarball/{sha}"
     tarball_path = f"{repo.replace('/', '-')}-{sha[:7]}"
     load_repo(tarball_url, dest)
     repo_metadata = {
+        "repo": repo,
         "sha": sha,
+        "ref": ref,
         # include the extracted folder path for the tarball
         # when retrieved from the GitHub API
         "tarball_path": tarball_path,
+        "tarball_url": tarball_url,
     }
     write_repo_metadata(repo, repo_metadata, dest)
     return repo_metadata
--- scaife_cts_api/data.py
+++ scaife_cts_api/data.py
@@ -20,24 +20,3 @@ def load_repo(tarball_url, dest):
     os.close(w)
     proc.wait()
 
-
-def resolve_commit(repo, ref):
-    if re.match(r"^[a-f0-9]{40}$", ref):
-        return ref
-    ref_url = f"https://api.github.com/repos/{repo}/git/refs/heads/{ref}"
-    headers = {
-        "Accept": "application/vnd.github.v3+json",
-    }
-    resp = requests.get(ref_url, headers=headers)
-    if resp.status_code == 404:
-        tag_url = f"https://api.github.com/repos/{repo}/git/tags/{ref}"
-        resp = requests.get(tag_url, headers=headers)
-        if resp.status_code == 404:
-            raise Exception(f"{repo}: ref ({ref}) not found")
-        else:
-            resp.raise_for_status()
-            ref_obj = resp.json()["object"]
-    else:
-        resp.raise_for_status()
-        ref_obj = resp.json()["object"]
-    return ref_obj["sha"]
